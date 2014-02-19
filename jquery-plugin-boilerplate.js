93a605b6e1be0520f73a26c7cb2284d28fd4c8cf
hellocreep <hellocreep@gmail.com>
2014-02-19 14:01:35 +0800

    init

---
+++ jquery-plugin-boilerplate.js
@@ -0,0 +1,43 @@
+;(function(factory) {
+	// AMD support
+	if(typeof define === 'function' && define.amd) {
+		define(['jquery'], factory);
+	} else {
+		factory(jQuery);
+	}
+})(function($) {
+	// Constructor
+	var Plugin = function(el, conf) {
+
+	}
+
+	// Prototype
+	Plugin.prototype.method = function() {
+
+	}
+
+	// Static property
+	Plugin.DEFAULTS = {
+
+	}
+
+	var old = $.fn.plugin;
+
+	// Plugin definition
+	$.fn.plugin = function(opts) {
+		return this.each(function() {
+			var $this = $(this),
+				data = $this.data('popover'),
+				conf = $.extend({}, Plugin.DEFAULTS, typeof opts === 'object' && opts);
+			
+			if (!data) $this.data('popover', (data = new Plugin(this, conf)));
+			if (typeof option == 'string') data[option]();
+		});
+	}
+
+	// No conflict
+	$.fn.plugin.noConflict = function() {
+		$.fn.plugin = old;
+		return this;
+	}
+});
